8e06dded8ddcbc41664df5a247e369d9dd1f30e1
Niv Levy <niv.levy@spacex.com>
2017-09-26 01:06:27 -0700

    fixed nema checksum encoding; now passed correctly through ac64a995ca47552890301bac1a5f3ec7325f4241 (alon xcsoar branch TopHat_uinput) still have to figure out the old xci file that allowed interaction with the nav bar, but arrows, enter, escape work properly.
    .

--- tophat_remote_serial_v0/tophat_remote_serial_v0.ino
+++ tophat_remote_serial_v0/tophat_remote_serial_v0.ino
@@ -66,22 +66,23 @@ void setup() {
 }
 
 // send button encoded as hex
-void send_coded_key(char k) {
+void send_coded_key(byte k) {
     byte checksum = 0;
     unsigned int i;
     String ps = String();
 
-    ps = ps + "$FAKBD," + String(k, HEX) + "*";
+    ps = ps + "$FAKBD," + String(k, HEX);
     // checksum calculation from https://rietman.wordpress.com/2008/09/25/how-to-calculate-the-nmea-checksum/
-    for (i=0; i< ps.length(); i++) {
+    for (i=1; i< ps.length(); i++) {
 	checksum ^= ps[i]; 
     }
+    ps = ps + "*" ;
     Serial.print(ps);
     Serial.println(checksum, HEX);
     return;
 }
 
-// note - i really want to send arrow keys, so we'll have to figure out how to do that, since it's not quite ascii; for now this is just checking real hardware; might need something in the driver, or can use more than one byte etc
+// raw uinpout values taken from https://github.com/torvalds/linux/blob/master/include/uapi/linux/input-event-codes.h
 
 void loop() {
   int i;
@@ -94,13 +95,13 @@ void loop() {
     int buttonJoystickState = digitalRead(buttonJoystick);
     // if the button state has changed, and it's currently pressed:
     if ((button1State != previousButton1State) && (button1State == LOW)) {
-      send_coded_key('4'); }
+      send_coded_key(5); } // KEY_4      5
     if ((button2State != previousButton2State)  && (button2State == LOW)) {
-      send_coded_key('6'); }
+      send_coded_key(7); } // KEY_6     7
     if ((button3State != previousButton3State)  && (button3State == LOW)) {
-      send_coded_key('8'); }
+      send_coded_key(1); } // KEY_ESC      1
     if ((buttonJoystickState != previousButtonJoystickState) && (buttonJoystickState == LOW)) {
-      send_coded_key('\n'); } // KEY_RETURN
+      send_coded_key(28); } // KEY_ENTER   28
     // save the current button state for comparison next time:
     previousButton1State = button1State;
     previousButton2State = button2State;
@@ -111,12 +112,12 @@ void loop() {
   int X = analogRead(JoystickX);
   int Y = analogRead(JoystickY);
   if (X < 256) 
-    send_coded_key('k'); 
+    send_coded_key(105); // KEY_LEFT    105
   if (X > 768) 
-    send_coded_key('j'); 
+    send_coded_key(106); //KEY_RIGHT    106
   if (Y < 256) 
-    send_coded_key('i'); 
+    send_coded_key(103); //KEY_UP      103
   if (Y > 768) 
-    send_coded_key('m');
+    send_coded_key(108); //KEY_DOWN    108
 }
 
